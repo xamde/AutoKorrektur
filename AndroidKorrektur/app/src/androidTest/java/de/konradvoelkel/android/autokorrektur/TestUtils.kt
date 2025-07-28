df55523104982cc4813f532ca3581e601fa030c4
Konrad Völkel <mail@konradvoelkel.com>
2025-07-28 23:31:34 +0200

    Remove unused import

--- AndroidKorrektur/app/src/androidTest/java/de/konradvoelkel/android/autokorrektur/TestUtils.kt
+++ AndroidKorrektur/app/src/androidTest/java/de/konradvoelkel/android/autokorrektur/TestUtils.kt
@@ -1,7 +1,6 @@
 package de.konradvoelkel.android.autokorrektur
 
 import android.content.Context
-import android.net.Uri
 import androidx.test.platform.app.InstrumentationRegistry
 import org.junit.Assert.fail
 import org.opencv.android.OpenCVLoader
